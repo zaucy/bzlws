1c76298e120d3a34fa57c2d09c9361f7821ce37c
Ezekiel Warren <zekewarren@gmail.com>
2020-01-21 19:29:29 -0800

    Initial rough implementation

---
+++ bzlws_copy/bzlws_copy.cc
@@ -0,0 +1,183 @@
+#include <filesystem>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+#include <locale>
+
+namespace fs = std::filesystem;
+
+void trim_ws(std::string& s) {
+	using std::isspace;
+	using std::find_if;
+
+	auto not_isspace = [](int c) { return !isspace(c); };
+	s.erase(s.begin(), find_if(s.begin(), s.end(), not_isspace));
+	s.erase(find_if(s.rbegin(), s.rend(), not_isspace).base(), s.end());
+}
+
+fs::path get_build_workspace_dir() {
+	char* bwd_env = std::getenv("BUILD_WORKSPACE_DIRECTORY");
+	if(bwd_env != nullptr) {
+		return fs::path(bwd_env).make_preferred();
+	}
+
+	std::cerr
+		<< "[ERROR] Missing 'BUILD_WORKSPACE_DIRECTORY' environment variable"
+		<< std::endl;
+	std::exit(1);
+}
+
+struct bazelignore_parse_results {
+	std::vector<fs::path> ignore_paths;
+
+	inline bool is_ignored_path
+		( const fs::path& path
+		)
+	{
+		auto pathPrefix = fs::absolute(path).make_preferred().string();
+
+		for(auto ignore_path : ignore_paths) {
+			auto ignorePathPrefix =
+				fs::absolute(ignore_path).make_preferred().string();
+
+			if(pathPrefix.rfind(ignorePathPrefix) == 0) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+};
+
+bazelignore_parse_results parse_bazelignore
+	( const fs::path& workspace_dir
+	)
+{
+	auto bazelignore_path = workspace_dir / ".bazelignore";
+
+	{
+		std::error_code ec;
+		auto bazelignore_exists = fs::exists(bazelignore_path, ec);
+		if(ec) {
+			std::cerr
+				<< "Error while checking if " << bazelignore_path << " exists: "
+				<< ec.message()
+				<< std::endl;
+			exit(1);
+		}
+
+		if(!bazelignore_exists) {
+			std::cerr
+				<< "[ERROR] " << bazelignore_path <<  " does not exist" << std::endl;
+			exit(1);
+		}
+	}
+
+	bazelignore_parse_results results;
+	std::ifstream bazelignore_file(bazelignore_path);
+
+	{
+		std::string line;
+		while(std::getline(bazelignore_file, line)) {
+			trim_ws(line);
+			if(!line.empty()) {
+				results.ignore_paths.push_back(workspace_dir / line);
+			}
+		}
+	}
+
+	return results;
+}
+
+std::vector<fs::path> get_src_paths
+	( const fs::path&  workspace_dir
+	, int              argc
+	, char**           argv
+	)
+{
+	std::vector<fs::path> src_paths;
+
+	for(int i=1; argc-1 > i; ++i) {
+		auto arg = std::string(argv[i]);
+		auto src_path = workspace_dir / arg;
+
+		if(!fs::exists(src_path)) {
+			std::cerr << "Source path does not exist: " << src_path << std::endl;
+			std::exit(1);
+		}
+
+		src_paths.push_back(src_path);
+	}
+
+	return src_paths;
+}
+
+fs::path get_out_path
+	( const fs::path&  workspace_dir
+	, int              argc
+	, char**           argv
+	)
+{
+	fs::path out_path = workspace_dir / std::string(argv[argc - 1]);
+
+	if(!fs::exists(out_path)) {
+		std::cerr << "Out path does not exist: " << out_path << std::endl;
+		std::exit(1);
+	}
+
+	if(!fs::is_directory(out_path)) {
+		std::cerr << "Out path is not a directory " << out_path << std::endl;
+		std::exit(1);
+	}
+
+	return out_path;
+}
+
+int main(int argc, char* argv[]) {
+
+	auto workspace_dir = get_build_workspace_dir();
+	auto bzlignore = parse_bazelignore(workspace_dir);
+	auto src_paths = get_src_paths(workspace_dir, argc, argv);
+	auto out_path = get_out_path(workspace_dir, argc, argv);
+
+	if(!bzlignore.is_ignored_path(out_path)) {
+		std::cerr
+			<< "[ERROR] bzlws_copy out path must be within your .bazelignore \""
+			<< out_path.string() << "\" does not reside in any of the following:"
+			<< std::endl;
+
+		for(auto ignore_path : bzlignore.ignore_paths) {
+			std::cerr
+				<< "  " << ignore_path.string() << std::endl;
+		}
+
+		std::exit(1);
+	}
+
+	for(auto src_path : src_paths) {
+		auto new_src_path = out_path / src_path.filename();
+
+		std::cout
+			<< fs::relative(src_path, workspace_dir).generic_string() << " -> "
+			<< fs::relative(new_src_path.string(), workspace_dir).generic_string()
+			<< std::endl;
+
+		if(fs::exists(new_src_path)) {
+			fs::remove(new_src_path);
+		}
+
+		std::error_code ec;
+		fs::copy_file(src_path, new_src_path, ec);
+		if(ec) {
+			std::cerr
+				<< "[ERROR] Failed to copy " << src_path.string() << " -> "
+				<< new_src_path.string() << std::endl << ec.message() << std::endl;
+			std::exit(1);
+		}
+	}
+
+	return 0;
+}
---
+++ index.bzl
@@ -0,0 +1,76 @@
+_sh_binary_suffix = "__bzlws_sh_binary_src"
+_sh_binary_contents = """
+#!/bin/bash
+
+# --- begin runfiles.bash initialization v2 ---
+# Copy-pasted from the Bazel Bash runfiles library v2.
+set -uo pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${{RUNFILES_DIR:-/dev/null}}/$f" 2>/dev/null || \\
+  source "$(grep -sm1 "^$f " "${{RUNFILES_MANIFEST_FILE:-/dev/null}}" | cut -f2- -d' ')" 2>/dev/null || \\
+  source "$0.runfiles/$f" 2>/dev/null || \\
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \\
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \\
+  {{ echo>&2 "ERROR: cannot find $f"; exit 1; }}; f=; set -e
+# --- end runfiles.bash initialization v2 ---
+
+tool_path=$(rlocation {tool})
+$tool_path {srcs_list_str} "{out_dir}"
+"""
+
+def _bzlws_tool_shell_script_src_impl(ctx):
+    name = ctx.attr.name
+    src_filename = name[:-len(_sh_binary_suffix)] + ".sh"
+    src = ctx.actions.declare_file(src_filename)
+    srcs_list_str = ""
+
+    for file in ctx.files.srcs:
+        srcs_list_str += "\"{}\"".format(file.path)
+
+    contents = _sh_binary_contents.format(
+        # tool = ctx.executable.tool.path,
+        tool = "bzlws/bzlws_copy/bzlws_copy.exe",
+        out_dir = ctx.attr.out_dir,
+        srcs_list_str = srcs_list_str,
+    )
+
+    ctx.actions.write(src, contents, True)
+    ctx.runfiles(files = [ctx.executable.tool])
+
+    return DefaultInfo(files = depset([src]))
+
+_bzlws_tool_shell_script_src = rule(
+    implementation = _bzlws_tool_shell_script_src_impl,
+    attrs = {
+        "srcs": attr.label_list(mandatory = True, allow_files = True),
+        "out_dir": attr.string(mandatory = True),
+        "tool": attr.label(
+            mandatory = True,
+            executable = True,
+            cfg = "host",
+        ),
+    },
+)
+
+def bzlw_copy(name = None, srcs = None, out_dir = None, visibility = None):
+
+    if out_dir.startswith("/"):
+        fail("out_dir cannot start with '/'")
+
+    tool = "@bzlws//bzlws_copy:bzlws_copy"
+
+    sh_script_name = name + _sh_binary_suffix
+    _bzlws_tool_shell_script_src(
+        name = sh_script_name,
+        srcs = srcs,
+        out_dir = out_dir,
+        tool = tool,
+        visibility = ["//visibility:private"],
+    )
+
+    native.sh_binary(
+        name = name,
+        srcs = [":" + sh_script_name],
+        deps = ["@bazel_tools//tools/bash/runfiles"],
+        data = [tool] + srcs,
+        visibility = visibility,
+    )
