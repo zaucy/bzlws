64627600edf888c6fa19322f283db0cbc73ccada
Ezekiel Warren <zekewarren@gmail.com>
2022-08-27 14:12:43 -0700

    bash test

--- test.sh
+++ test.sh
@@ -2,3 +2,14 @@
 
 set -e
 
+function test_path_or_exit {
+	if [ ! -f "$1" ]; then
+		echo "Test FAILED: '$1' does not exist"
+	fi
+}
+
+bazel run //example:copy_example_txt
+test_path_or_exit ./example/ignored_folder/example.txt
+
+bazel run //example:copy_example_txt_file_path
+test_path_or_exit ./example/ignored_folder/example/example.txt
