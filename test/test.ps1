cabe24947c7bfa260ecc72a21a1617e31cd349fa
Ezekiel Warren <zekewarren@gmail.com>
2024-07-03 21:22:32 -0700

    add bazelignore to test dir

--- test/test.ps1
+++ test/test.ps1
@@ -9,19 +9,19 @@ function Test-Path-Or-Exit($Path) {
 	}
 }
 
-git clean -fx example
-bazel build //example/...
+git clean -fx .
+bazel build //...
 
-bazel run --config=test //example:copy_example_txt
-Test-Path-Or-Exit .\example\ignored_folder\example.txt
+bazel run --config=test //:copy_example_txt
+Test-Path-Or-Exit .\ignored_folder\example.txt
 
-bazel run --config=test //example:copy_example_txt_file_path
-Test-Path-Or-Exit .\example\ignored_folder\example\example.txt
+bazel run --config=test //:copy_example_txt_file_path
+Test-Path-Or-Exit .\ignored_folder\example\example.txt
 
-bazel run --config=test //example:copy_example_txt_file_path_external
-Test-Path-Or-Exit .\example\ignored_folder\external\faux_repo\example.txt
+bazel run --config=test //:copy_example_txt_file_path_external
+Test-Path-Or-Exit .\ignored_folder\external\faux_repo\example.txt
 
-git clean -fx example
+git clean -fx .
 
-bazel run --config=test --config=ci //example:copy_example_txt_file_path_external
-Test-Path-Or-Exit .\example\ignored_folder\external\faux_repo\example.txt
+bazel run --config=test --config=ci //:copy_example_txt_file_path_external
+Test-Path-Or-Exit .\\ignored_folder\external\faux_repo\example.txt
